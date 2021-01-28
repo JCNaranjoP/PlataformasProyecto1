37867a1c6569dc7fbbdf0630981f23e9b86e70b6
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-01-28 00:57:10 -0500

    Declarate var

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+
+/*Var declaration*/
+var form_login = document.querySelector(".form_login");
+var form_sing_up = document.querySelector(".form_sing-up");
+var container_login_sing_up = document.querySelector(".container_login-sing-up")
+var box_login = document.querySelector(".box_login");
+var box_sing_up = document.querySelector(".box_sing-up");
+
