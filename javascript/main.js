6303f472b5df108ac3e7517a2e9c6e6691bdb485
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-01-28 01:03:05 -0500

    create function register

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,10 @@ var container_login_sing_up = document.querySelector(".container_login-sing-up")
 var box_login = document.querySelector(".box_login");
 var box_sing_up = document.querySelector(".box_sing-up");
 
+function register(){
+form_sing_up.style.display = "block";
+container_login_sing_up.style.left = "410px";
+form_login.style.display = "none";
+box_sing_up.style.opacity = "0";
+box_login.style.opacity = "1";
+}
