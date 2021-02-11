6dd7d7458f661a5f67334fca5750d9ba66b8c375
JCNaranjoP <juan.naranjop@upb.edu.co>
2021-02-11 00:04:20 -0500

    function btn form f/c password

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 document.getElementById("btn_sing-up").addEventListener("click", register);
 document.getElementById("btn_login").addEventListener("click", login);
 window.addEventListener("resize",widthPage);
+document.getElementById("forgot-password").addEventListener("click",forgot_password);
+document.getElementById("btn_f-password").addEventListener("click", change_password);
+document.getElementById("btn_c-password").addEventListener("click",change);
 
 /*Var declaration*/
 var form_login = document.querySelector(".form_login");
@@ -12,6 +15,48 @@ var container_login_sing_up = document.querySelector(".container_login-sing-up")
 var box_login = document.querySelector(".box_login");
 var box_sing_up = document.querySelector(".box_sing-up");
 
+
+function change(){
+    alert("Contraseña restablecida");
+    login();
+}
+
+
+function change_password(){
+    if(window.innerWidth > 850){
+        form_f_password.style.display = "none";
+        form_c_password.style.display = "block";
+    }else{
+    form_sing_up.style.display = "none";
+    container_login_sing_up.style.left = "0px";
+    form_login.style.display = "none";
+    box_sing_up.style.display = "block";
+    box_login.style.display = "none";
+    form_f_password.style.display = "none";
+    form_c_password.style.display = "block";
+    }
+}
+
+
+function forgot_password(){
+    if(window.innerWidth > 850){
+        form_sing_up.style.display = "none";
+        container_login_sing_up.style.left = "10px";
+        form_login.style.display = "none";
+        box_sing_up.style.opacity = "1";
+        box_login.style.opacity = "0";
+        form_f_password.style.display = "block";
+        form_c_password.style.display = "none";
+    }else{
+    form_sing_up.style.display = "none";
+    container_login_sing_up.style.left = "0px";
+    form_login.style.display = "none";
+    box_sing_up.style.display = "block";
+    box_login.style.display = "none";
+    form_f_password.style.display = "block";
+    form_c_password.style.display = "none";
+    }
+}
 
 function register(){
     if(window.innerWidth > 850){
